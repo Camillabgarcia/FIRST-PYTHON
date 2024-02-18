c311e7060b50a5b9b82e337426c55fc532c46c34
Camillabgarcia <camillabgarciaa@hotmail.com>
2024-02-18 09:04:36 -0300

    Tipo int

--- app.py
+++ app.py
@@ -8,11 +8,20 @@ print("""
       """)  #\n: Pular linha
 
 print('1.Cadastrar restaurante')     #Print: exibir informação.
-print('2.Listar restaurante')
+print('2.Listar restaurantes')
 print('3.Ativar restaurante')
 print('4.Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')               #INPUT: puxar informação do usuário.
-print(f'Você escolheu a opção {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma opção: '))    #INPUT: puxar informação do usuário.  #INT: transformar o resultado de string para número inteiro.
+  
+
+if opcao_escolhida == 1 :        # == COMPARAÇÃO.
+    print ('Cadastrar restaurante')
+elif opcao_escolhida == 2 :           # ELIF : else.
+    print ('Listar restaurante')
+elif opcao_escolhida == 3 :          
+    print ('Ativar restaurante')
+else:          
+    print ('Encerrando o programa')
 
 

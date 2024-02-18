7d93774260c1312621abf3c30ca3a234a47004c3
Camillabgarcia <camillabgarciaa@hotmail.com>
2024-02-18 09:20:14 -0300

    Implementando função no código

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+
+
+
 print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -5,7 +9,7 @@ print("""
 ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
-      """)  #\n: Pular linha
+      """)                              #(""" """): Pular várias linhas.
 
 print('1.Cadastrar restaurante')     #Print: exibir informação.
 print('2.Listar restaurantes')
@@ -13,15 +17,18 @@ print('3.Ativar restaurante')
 print('4.Sair\n')
 
 opcao_escolhida = int(input('Escolha uma opção: '))    #INPUT: puxar informação do usuário.  #INT: transformar o resultado de string para número inteiro.
-  
+
+def finalizar_app():                                          #DEF: função
+     os.system('cls')                         #Limpando a tela quando cair na opção 4.
+     print('Finalizando o app\n')           #\n: pulando linha.
 
 if opcao_escolhida == 1 :        # == COMPARAÇÃO.
     print ('Cadastrar restaurante')
 elif opcao_escolhida == 2 :           # ELIF : else.
     print ('Listar restaurante')
-elif opcao_escolhida == 3 :          
-    print ('Ativar restaurante')
+elif opcao_escolhida == 3 :
+     print ('Ativar restaurante')        
 else:          
-    print ('Encerrando o programa')
+    finalizar_app()
 
 

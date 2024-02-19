aed5c5a077c6c05af6dc018f702acb2585854b82
Camillabgarcia <camillabgarciaa@hotmail.com>
2024-02-19 17:53:08 -0300

    Novos Restaurantes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os     #Chamando algum comando da biblioteca.
 
-restaurantes = []
+restaurantes = ['Pizzaria', 'Churrascaria']
 
 def exibir_nome_do_programa():
     print("""
@@ -33,8 +33,19 @@ def cadastrar_novo_restaurante():
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)                         #colocar o nome dentro da lista   
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('Digite uma tecla para voltar ao menu inicial.')
-    main()          
+    input('\nDigite uma tecla para voltar ao menu inicial.')
+    main()      
+
+def listar_resturantes():
+    os.system('cls')
+    print('Listando os restaurantes\n')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    input('\nDigite uma tecla para voltar ao menu inicial.')
+    main()
+
 
 def escolher_opcao():
     try:
@@ -43,7 +54,7 @@ def escolher_opcao():
         if opcao_escolhida == 1 :        # == COMPARAÇÃO.
             cadastrar_novo_restaurante()
         elif opcao_escolhida == 2 :           # ELIF : else.
-            print ('Listar restaurante')
+            listar_resturantes()
         elif opcao_escolhida == 3 :
             print ('Ativar restaurante')
         elif opcao_escolhida == 4 :

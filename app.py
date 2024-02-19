082c716f28dfab27bc93504e62a81d311a6bf7f5
Camillabgarcia <camillabgarciaa@hotmail.com>
2024-02-19 16:51:41 -0300

    Try except

--- app.py
+++ app.py
@@ -21,24 +21,33 @@ def finalizar_app():                                          #DEF: função
     os.system('cls')                         #Limpando a tela quando cair na opção 4.
     print('Finalizando o app\n')           #\n: pulando linha.
 
+def opcao_invalida():
+    print('Opção inválida\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))    #INPUT: puxar informação do usuário.  #INT: transformar o resultado de string para número inteiro.
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))    #INPUT: puxar informação do usuário.  #INT: transformar o resultado de string para número inteiro.
 
-
-    if opcao_escolhida == 1 :        # == COMPARAÇÃO.
-        print ('Cadastrar restaurante')
-    elif opcao_escolhida == 2 :           # ELIF : else.
-        print ('Listar restaurante')
-    elif opcao_escolhida == 3 :
-        print ('Ativar restaurante')        
-    else:          
-        finalizar_app()
+        if opcao_escolhida == 1 :        # == COMPARAÇÃO.
+            print ('Cadastrar restaurante')
+        elif opcao_escolhida == 2 :           # ELIF : else.
+            print ('Listar restaurante')
+        elif opcao_escolhida == 3 :
+            print ('Ativar restaurante')
+        elif opcao_escolhida == 4 :
+            finalizar_app()   
+        else:          
+            opcao_invalida()
+    except:
+        opcao_invalida()        
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
 
 if __name__ == '__main__':     #Nós não queremos que ele seja importado por outros arquivos.
     main()
-    
